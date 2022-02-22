c77b992bbfec21851ee20a1012918e42e89c63d0
Marcelo <marceloabreu1997@gmail.com>
2022-02-22 11:17:13 -0300

    @m-arceloabreu: Projeto Maven sem Dependencias

---
+++ src/main/java/org/generation/placeplus/PlaceplusApplication.java
@@ -0,0 +1,13 @@
+package org.generation.placeplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceplusApplication.class, args);
+	}
+
+}

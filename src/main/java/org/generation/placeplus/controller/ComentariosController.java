f71988280ddd3cdc708f1c95e70d9593206589bb
Marcelo <marceloabreu1997@gmail.com>
2022-03-03 12:48:35 -0300

    @m-arceloabreu: [Criação de Classes]

---
+++ src/main/java/org/generation/placeplus/controller/ComentariosController.java
@@ -0,0 +1,5 @@
+package org.generation.placeplus.controller;
+
+public class ComentariosController {
+
+}
---
+++ src/main/java/org/generation/placeplus/repository/PostagemRepository.java
@@ -0,0 +1,8 @@
+package org.generation.placeplus.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostagemRepository {
+
+}

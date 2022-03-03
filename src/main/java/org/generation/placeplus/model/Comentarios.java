94d058fa528794efbaf0fa085943568aac4d204e
Luciana <lucianat.s@hotmail.com>
2022-03-03 14:03:35 -0300

    @lucianaTSoares [Task: Update model Comentarios]

--- src/main/java/org/generation/placeplus/model/Comentarios.java
+++ src/main/java/org/generation/placeplus/model/Comentarios.java
@@ -1,5 +1,92 @@
 package org.generation.placeplus.model;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_comentarios")
 public class Comentarios {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idCom;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+
+    @NotNull
+    @Size(min = 1, max = 255)
+    private String texto;
+    
+    @NotNull
+    private boolean sensivel;
+
+    @ManyToOne
+    @JsonIgnoreProperties("comentarios")
+    private Postagem postagem;
+
+    @ManyToOne
+    @JsonIgnoreProperties("comentarios")
+    private Usuario usuario;
+
+    public int getIdCom() {
+        return idCom;
+    }
+
+    public void setIdCom(int idCom) {
+        this.idCom = idCom;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public boolean isSensivel() {
+        return sensivel;
+    }
+
+    public void setSensivel(boolean sensivel) {
+        this.sensivel = sensivel;
+    }
+
+    public Postagem getPostagem() {
+        return postagem;
+    }
+
+    public void setPostagem(Postagem postagem) {
+        this.postagem = postagem;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }

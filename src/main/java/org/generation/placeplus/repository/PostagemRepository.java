e03fd6a9575f07933033ce12086e839f064f31cc
Pedro-Marcos1223 <pedromarcospdm@gmail.com>
2022-03-03 13:27:47 -0300

    @Pedro-Marcos1223 [Task: Add PostagemRepository]

--- src/main/java/org/generation/placeplus/repository/PostagemRepository.java
+++ src/main/java/org/generation/placeplus/repository/PostagemRepository.java
@@ -1,8 +1,14 @@
 package org.generation.placeplus.repository;
 
+import java.util.List;
+
+import org.generation.placeplus.model.Postagem;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostagemRepository {
 
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	
+	public List<Postagem> findAllByTemaContainingIgnoreCase(String tema);
 }

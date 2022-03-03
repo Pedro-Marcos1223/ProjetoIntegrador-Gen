572ee9aab2baf61652f870cff8ae0acc8195bf96
Marcelo <marceloabreu1997@gmail.com>
2022-03-03 16:05:42 -0300

    @m-arceloabreu:[PostagemController PUT AND DELETE]

--- src/main/java/org/generation/placeplus/controller/PostagemController.java
+++ src/main/java/org/generation/placeplus/controller/PostagemController.java
@@ -10,9 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,5 +52,13 @@ public class PostagemController {
 	@PostMapping
 	public ResponseEntity<Postagem> post (@Valid @RequestBody Postagem postagem){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	@PutMapping
+	public ResponseEntity<Postagem> put (@Valid @RequestBody Postagem postagem){
+		return ResponseEntity.ok(repository.save(postagem));		
+	}
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable int id) {
+		repository.deleteById(id);
 	}
 }

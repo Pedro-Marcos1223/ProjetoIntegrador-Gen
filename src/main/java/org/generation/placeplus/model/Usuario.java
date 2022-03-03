bff05e448c15960057ccc8edeb9392866308533c
Luciana <lucianat.s@hotmail.com>
2022-03-03 14:04:45 -0300

    @lucianaTSoares [Task: Add anotation OneToMany]

--- src/main/java/org/generation/placeplus/model/Usuario.java
+++ src/main/java/org/generation/placeplus/model/Usuario.java
@@ -1,12 +1,18 @@
 package org.generation.placeplus.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_usuario")
@@ -32,6 +38,10 @@ public class Usuario {
 
 	@Size(max = 45)
 	private String pronome;
+
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("usuario")
+	private List<Comentarios> comentarios;
 
 	public int getIdUsuario() {
 		return idUsuario;

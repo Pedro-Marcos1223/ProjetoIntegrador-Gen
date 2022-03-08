26ff3aa0c9893917d5b4bd50801b734604113b0c
Diego Sérgio <diegooosergio@outlook.com>
2022-03-08 14:53:24 -0300

    @DiegoBelo1 [Erro Desconhecido]

--- src/main/java/org/generation/placeplus/service/UsuarioService.java
+++ src/main/java/org/generation/placeplus/service/UsuarioService.java
@@ -16,38 +16,34 @@ public class UsuarioService {
 
 	@Autowired
 	private UsuarioRepository repository;
-	
+
 	public Usuario cadastrarUsuario(Usuario usuario) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		 
-		
+
 		String senhaEncoder = encoder.encode(usuario.getSenha());
 		usuario.setSenha(senhaEncoder);
 		return repository.save(usuario);
 	}
 
-	public Optional<UsuarioLogin> Logar(Optional<UsuarioLogin> user){
+	public Optional<UsuarioLogin> Logar(Optional<UsuarioLogin> user) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
-		
+
 		if (usuario.isPresent()) {
-			if(encoder.matches(user.get().getSenha(),usuario.get().getSenha())) {
+			if (encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
 				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
 				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
 				String authHeader = "Basic " + new String(encodedAuth);
-				
-				
-			user.get().setToken(authHeader);
-			user.get().setNome(usuario.get().getNome());
-			return user;
-				
+
+				user.get().setToken(authHeader);
+				user.get().setNome(usuario.get().getNome());
+				user.get().setSenha(usuario.get().getSenha());
+				return user;
+
 			}
 		}
-	return null;
-		
-		
+		return null;
+
 	}
-	
-	
-	
+
 }

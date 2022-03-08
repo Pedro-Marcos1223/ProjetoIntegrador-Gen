c7d5f1ee7bc0829c3ddfc90e140c59ac9e945693
Raysa Souza <raysafiigueiredo@gmail.com>
2022-03-08 14:12:10 -0300

    @RAYSAFIGUEIREDO CRIEI A CLASSE USUARIO SERVICE

--- src/main/java/org/generation/placeplus/service/UsuarioService.java
+++ src/main/java/org/generation/placeplus/service/UsuarioService.java
@@ -1,6 +1,11 @@
 package org.generation.placeplus.service;
 
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.apache.commons.codec.binary.Base64;
 import org.generation.placeplus.model.Usuario;
+import org.generation.placeplus.model.UsuarioLogin;
 import org.generation.placeplus.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,7 +18,7 @@ public class UsuarioService {
 	private UsuarioRepository repository;
 	
 	public Usuario cadastrarUsuario(Usuario usuario) {
-		BCryptPasswordEncoder encouder = new BCryptPasswordEncoder();
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		 
 		
 		String senhaEncoder = encoder.encode(usuario.getSenha());
@@ -21,12 +26,24 @@ public class UsuarioService {
 		return repository.save(usuario);
 	}
 
-	
-	
-	
-	
-	
-	
+	public Optional<UsuarioLogin> Logar(Optional<UsuarioLogin> user){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+		
+		if (usuario.isPresent()) {
+			if(encoder.matches(user.get().getSenha(),usuario.get().getSenha())) {
+				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
+				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodedAuth);
+				
+				
+			user.get().setToken(authHeader);
+			user.get().setNome(usuario.get().getNome());
+			return user;
+				
+			}
+		}
+	return null;
 		
 		
 	}

d1de72355538dd7accbfefd3f2a917f877bcac0b
Marcelo <marceloabreu1997@gmail.com>
2022-04-10 12:33:50 -0300

    @m-arceloabreu: task:[Alterações no Back, Put User e mais]

--- src/main/java/org/generation/placeplus/service/UsuarioService.java
+++ src/main/java/org/generation/placeplus/service/UsuarioService.java
@@ -8,8 +8,10 @@ import org.generation.placeplus.model.Usuario;
 import org.generation.placeplus.model.UsuarioLogin;
 import org.generation.placeplus.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UsuarioService {
@@ -17,12 +19,12 @@ public class UsuarioService {
 	@Autowired
 	private UsuarioRepository repository;
 
-	public Usuario cadastrarUsuario(Usuario usuario) {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+	public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
 
-		String senhaEncoder = encoder.encode(usuario.getSenha());
-		usuario.setSenha(senhaEncoder);
-		return repository.save(usuario);
+		if(repository.findByUsuario(usuario.getUsuario()).isPresent()) 
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
+		usuario.setSenha(criptografarSenha(usuario.getSenha()));
+		return Optional.of(repository.save(usuario));
 	}
 
 	public Optional<UsuarioLogin> Logar(Optional<UsuarioLogin> user) {
@@ -40,6 +42,7 @@ public class UsuarioService {
 				user.get().setId(usuario.get().getId());
 				user.get().setFoto(usuario.get().getFoto());
 				user.get().setTipo(usuario.get().getTipo());
+				user.get().setBio(usuario.get().getBio());
 				user.get().setSenha(usuario.get().getSenha());
 				return user;
 
@@ -48,5 +51,39 @@ public class UsuarioService {
 		return null;
 
 	}
+	
+	public Optional<Usuario> atualizarUsuario(Usuario usuario) {
 
+		if (repository.findById(usuario.getId()).isPresent()) {
+			Optional<Usuario> buscaUsuario = repository.findByUsuario(usuario.getUsuario());
+
+			if (buscaUsuario.isPresent()) {				
+				if (buscaUsuario.get().getId() != usuario.getId())
+					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
+			}
+			
+			usuario.setSenha(criptografarSenha(usuario.getSenha()));
+
+			return Optional.of(repository.save(usuario));
+		} 
+			
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado!", null);		
+	}
+	
+	private String criptografarSenha(String senha) {
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String senhaEncoder = encoder.encode(senha);
+
+		return senhaEncoder;
+	}
+	
+	private String generatorBasicToken(String email, String password) {
+		String structure = email + ":" + password;
+		byte[] structureBase64 = Base64.encodeBase64(structure.getBytes(Charset.forName("US-ASCII")));
+		return "Basic " + new String(structureBase64);
+	}
+	
+	
+	
 }
